48f0eb2f732bd8c25c2a2e5a030103535157cbd1
Sreedhar9652002484 <nagallisreedhar1918.com>
2023-08-28 11:27:15 +0530

    [Sreedhar]:Deployed FunDooProject

--- FunDooNote/Startup.cs
+++ FunDooNote/Startup.cs
@@ -137,10 +137,10 @@ namespace FunDooNoteApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
